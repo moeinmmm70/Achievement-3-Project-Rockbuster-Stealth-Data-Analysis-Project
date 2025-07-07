3c3d7285283d5df49baf5363d7f6910b82f4cc2a
Moein Mellat <71269866+moeinmmm70@users.noreply.github.com>
2025-07-07 11:46:50 +0200

    1_filtering_data.sql

--- 1_filtering_data.sql
+++ 1_filtering_data.sql
@@ -1,1 +1,15 @@
+-- SQL Query: Ordering the Data
 
+SELECT *
+FROM film
+ORDER BY title ASC, release_year DESC, rental_rate DESC;
+
+-- SQL Query: Grouping Data by Rating
+
+SELECT
+rating,
+AVG(rental_rate) AS avg_rental_rate,
+MIN(rental_duration) AS min_rental_duration,
+MAX(rental_duration) AS max_rental_duration
+FROM film
+GROUP BY rating;
